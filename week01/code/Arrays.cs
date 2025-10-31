66488bad57fc79fc55820628e9d2584e13f6a84e
Abraham <saulabraham881@gmail.com>
2025-10-30 21:57:56 -0600

    I did all the tasks

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Xml.XPath;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,20 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+
+        //I will create an array of doubles with the size of length
+        var multiples = new double[length];
+        //I will use a for loop to iterate through the array and assign each index the value of number multiplied by the index + 1
+        for (int i = 0; i < length; i++)
+        {
+
+            //At each index, I will assign the value of number multiplied by (i + 1) to account for the fact that arrays are 0-indexed
+            multiples[i] = number * (i + 1);
+        }
+
+
+
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -25,6 +40,18 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
+        // I will get the last 'amount' of elements from the list and store them in a new list called 'endpart'
+        var endpart = data.GetRange(data.Count - amount, amount);
+        // I will get the first part of the list up to the point where 'endpart' starts and store it in a new list called 'startpart'
+        var startpart = data.GetRange(0, data.Count - amount);
+        // I will clear the original list and then add the elements from 'endpart' followed by the elements from 'startpart'
+        data.Clear();
+        //I will add the elements from 'endpart' to the original list
+        data.AddRange(endpart);
+
+        //I will add the elements from 'startpart' to the original list
+        data.AddRange(startpart);
+        
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could

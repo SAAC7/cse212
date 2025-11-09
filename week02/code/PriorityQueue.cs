f2cefd0649e3038586aa70e76bd3e7466662def5
Abraham <saulabraham881@gmail.com>
2025-11-09 01:44:39 -0600

    i finish the test

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -9,6 +9,7 @@
     /// </summary>
     /// <param name="value">The value</param>
     /// <param name="priority">The priority</param>
+    public int Count => _queue.Count;
     public void Enqueue(string value, int priority)
     {
         var newNode = new PriorityItem(value, priority);
@@ -24,14 +25,15 @@
 
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 1; index < _queue.Count; index++)
         {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
                 highPriorityIndex = index;
         }
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex);
         return value;
     }
 
--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,23 +6,56 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: try adding multiple items with different priorities and dequeue all of them
+    // Expected Result: items should be dequeued in order of their priority (highest first)
     // Defect(s) Found: 
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var items = new (string name, int priority)[]
+        {
+            ("task1", 1), ("task2", 3), ("task3", 2), ("task4", 5), ("task5", 4)
+        };
+        foreach (var (name, priority) in items)
+        {
+            priorityQueue.Enqueue(name, priority);
+        }
+
+        var dequeuedItems = new List<string>();
+        while (priorityQueue.Count > 0)
+        {
+            dequeuedItems.Add(priorityQueue.Dequeue());
+        }
+
+        var expectedOrder = new List<string> { "task4", "task5", "task2", "task3", "task1" };
+        CollectionAssert.AreEqual(expectedOrder, dequeuedItems);
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: try adding multiple items with the same priority and dequeue all of them
+    // Expected Result: items should be dequeued in the order they were added
     // Defect(s) Found: 
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var items = new (string name, int priority)[]
+        {
+            ("task1", 1), ("task2", 1), ("task3", 1)
+        };
+        foreach (var (name, priority) in items)
+        {
+            priorityQueue.Enqueue(name, priority);
+        }
+        Console.WriteLine(priorityQueue.ToString());
+        Console.WriteLine(priorityQueue.Count);
+        var dequeuedItems = new List<string>();
+        while (priorityQueue.Count > 0)
+        {
+            dequeuedItems.Add(priorityQueue.Dequeue());
+        }
+
+        var expectedOrder = new List<string> { "task1", "task2", "task3" };
+        CollectionAssert.AreEqual(expectedOrder, dequeuedItems);
     }
 
     // Add more test cases as needed below.
